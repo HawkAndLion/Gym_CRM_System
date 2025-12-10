333ed2f2141368adc134a11cb86fd6ae37923451
Nadya Lee <nadyalee1985@gmail.com>
2025-12-11 00:27:28 +0500

    update uri in TrainerWorkloadInterface

--- src/main/java/learn/epam/com/feign/TrainerWorkloadInterface.java
+++ src/main/java/learn/epam/com/feign/TrainerWorkloadInterface.java
@@ -7,16 +7,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-@FeignClient("TRAINER-WORKLOAD-SERVICE")
+@FeignClient(name = "trainer-workload-service", url = "http://localhost:8082")
 public interface TrainerWorkloadInterface {
-    @PostMapping
-    public ResponseEntity<Void> processTrainingEvent(@RequestBody TrainingEventDto dto,
-                                                     @RequestHeader(value = "transactionId", required = false) String transactionId);
 
+    @PostMapping("/api/v1/workload")
+    void processTrainingEvent(@RequestBody TrainingEventDto dto,
+                              @RequestHeader("Authorization") String bearerToken);
 
-    @GetMapping("/{username}/{year}/{month}")
-    public ResponseEntity<TrainingSummaryDto> getMonthlySummary(@PathVariable String username,
-                                                                @PathVariable int year,
-                                                                @PathVariable int month,
-                                                                @RequestHeader(value = "transactionId", required = false) String transactionId);
+    @GetMapping("/api/v1/workload/{username}/{year}/{month}")
+    ResponseEntity<TrainingSummaryDto> getMonthlySummary(@PathVariable String username,
+                                                         @PathVariable int year,
+                                                         @PathVariable int month,
+                                                         @RequestHeader("Authorization") String bearerToken);
 }
