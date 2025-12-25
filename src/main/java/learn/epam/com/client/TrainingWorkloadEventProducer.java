dec93ea716b2f66da8513ec2d655943191938500
Nadya Lee <nadyalee1985@gmail.com>
2025-12-25 15:18:32 +0500

    update TrainingWorkloadEventProducer on Security

--- src/main/java/learn/epam/com/client/TrainingWorkloadEventProducer.java
+++ src/main/java/learn/epam/com/client/TrainingWorkloadEventProducer.java
@@ -1,8 +1,9 @@
 package learn.epam.com.client;
 
 import learn.epam.com.dto.client.TrainingEventDto;
+import learn.epam.com.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
@@ -11,12 +12,26 @@ import org.springframework.stereotype.Component;
 public class TrainingWorkloadEventProducer {
 
     private static final String DESTINATION = "trainer.workload.queue";
+    private static final String AUTHORIZATION = "Authorization";
+    private static final String BEARER = "Bearer ";
 
-    @Autowired
     private final JmsTemplate jmsTemplate;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Value("${app.service.name}")
+    private String serviceName;
 
     public void send(TrainingEventDto dto) {
-        jmsTemplate.convertAndSend(DESTINATION, dto);
+        String serviceToken =
+                jwtTokenProvider.generateServiceToken(serviceName);
+
+        jmsTemplate.convertAndSend(DESTINATION, dto, message -> {
+            message.setStringProperty(
+                    AUTHORIZATION,
+                    BEARER + serviceToken
+            );
+            return message;
+        });
     }
 }
 
